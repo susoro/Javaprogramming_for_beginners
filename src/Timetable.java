5d069d7a36fd4de79118cc4e06449f1aa09c7a90
sugyeongsu <78544372+susoro@users.noreply.github.com>
2024-06-24 23:30:02 +0900

    구구단 5단 완성

---
+++ src/Timetable.java
@@ -0,0 +1,12 @@
+public class Timetable {
+
+    public static void main(String[] args) {
+        var cal = 5;
+
+        System.out.println(cal*5);
+        //구구단 5단
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i+"*5는?"+ i*5);
+        }
+    }
+}
